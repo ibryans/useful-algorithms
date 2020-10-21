2d358ca5d9f58666fa10401e9995a9700a6e801f
Fábio Marra <fabiomarrafilho@yahoo.com.br>
2020-10-21 12:34:59 -0300

    Correct this code

--- JavaScript/useful_JS_functions.js
+++ JavaScript/useful_JS_functions.js
@@ -1,8 +1,8 @@
-export const integerProperties = (number) => {
-  number = Number.floor(number);
+e_primo_ou_nao(number) => {
+  number = Math.floor(number);
   let dividedBy = [1, number];
   let prime = true;
-  const limit = Number.sqrt(number);
+  const limit = Math.sqrt(number);
   for (let i = 2; i <=limit; i++) {
     if(!(number%i)) {
       prime = false;
@@ -14,3 +14,4 @@ export const integerProperties = (number) => {
     dividedBy: dividedBy
   }
 };
+
